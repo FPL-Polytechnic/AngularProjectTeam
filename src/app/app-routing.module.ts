f5639d9b1dcc7c96858793efc0842c8abc68e242
Tran Hai Duong <haiduong12122003@gmail.com>
2023-06-03 17:19:30 +0700

    feature: [FE]-Register-Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterUserComponent } from './auth/register-user/register-user.component';
+import { LoginUserComponent } from './auth/login-user/login-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterUserComponent },
+  { path: 'login', component: LoginUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http"
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { RegisterUserComponent } from './auth/register-user/register-user.component';
+import { LoginUserComponent } from './auth/login-user/login-user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, ProductsListComponent, RegisterUserComponent, LoginUserComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
