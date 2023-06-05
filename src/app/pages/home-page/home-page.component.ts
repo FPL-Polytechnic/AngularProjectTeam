5460f450edbb61a0a4d9393e858e14160deda144
huy18032003 <vhuy18032003@gmail.com>
2023-06-05 19:16:31 +0700

    feat:APT-26-list-add-home-product
    refs:APT-26

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ICategory } from 'src/app/interfaces/category';
+import { IProduct } from 'src/app/interfaces/product';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
+  products: IProduct[] = [];
+  categories: ICategory[] = [];
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService
+  ) {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data.data.docs;
+    });
+    this.categoryService.getCategories().subscribe((data) => {
+      this.categories = data.data;
+    });
+  }
+  categoryName(id: any) {
+    const category = this.categories.find((item) => item._id === id);
+    return category?.name;
+  }
+
 
 }
