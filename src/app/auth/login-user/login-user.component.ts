1acd09527cc58335f7183b261dd77f2fd2bda121
Tran Hai Duong <haiduong12122003@gmail.com>
2023-06-08 21:01:57 +0700

    fix : Login

--- src/app/auth/login-user/login-user.component.ts
+++ src/app/auth/login-user/login-user.component.ts
@@ -6,32 +6,28 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.component.html',
-  styleUrls: ['./login-user.component.scss']
+  styleUrls: ['./login-user.component.scss'],
 })
 export class LoginUserComponent {
   loginForm: FormGroup;
-  constructor(
-    private userService: AuthService,
-    private router: Router
-  ) { 
+  constructor(private userService: AuthService, private router: Router) {
     this.loginForm = new FormGroup({
-      email: new FormControl('',[Validators.email, Validators.required]),
-      password: new FormControl('',[Validators.required])
-    })
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onSubmit(){
+  onSubmit() {
     const data = this.loginForm.value;
     this.userService.login(data).subscribe((user) => {
-      console.log(user);        
-        localStorage.setItem('accessToken', JSON.stringify(user))
-        alert("Đăng nhập thành công")
-        setTimeout(()=>{
-          this.router.navigateByUrl('/')
-        }, 1000)    
-    })
+      // console.log(user);
+      localStorage.setItem('accessToken', JSON.stringify(user));
+      alert('Đăng nhập thành công✅');
+      setTimeout(() => {
+        this.router.navigateByUrl('/');
+      }, 1000);
+    });
   }
 }
