ba184d902d972f6232bcf7391f0cf594cb399e61
Tran Hai Duong <haiduong12122003@gmail.com>
2023-06-09 20:45:37 +0700

    fix : Check before sending

--- src/app/pages/admin/user-list/user-list.component.ts
+++ src/app/pages/admin/user-list/user-list.component.ts
@@ -5,16 +5,13 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent {
-  user: IUser []=[]
-  constructor(
-    private productsUser : UserService
-  ){
-   this.productsUser.getUsers().subscribe((data) =>{  
-    this.user =data.data;
-  
-   })
+  user: IUser[] = [];
+  constructor(private productsUser: UserService) {
+    this.productsUser.getUsers().subscribe((data) => {
+      this.user = data.data;
+    });
   }
 }
--- src/app/showvalidate/showvalidate.component.ts
+++ src/app/showvalidate/showvalidate.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit  } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-showvalidate',
   templateUrl: './showvalidate.component.html',
-  styleUrls: ['./showvalidate.component.scss']
+  styleUrls: ['./showvalidate.component.scss'],
 })
 export class ShowvalidateComponent {
   @Input() field: any;
   @Input() key: string;
-  constructor() { 
-    this.key = ''
+  constructor() {
+    this.key = '';
   }
 }
