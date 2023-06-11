1c6bbc1b59f841b9900325267eb6b09ca96b7e08
Tran Hai Duong <haiduong12122003@gmail.com>
2023-06-11 21:15:03 +0700

    feat: Send Token Server
    refs : APT-69

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
- 
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     const userInfo = this.authService.isAuthenticated();
     if (userInfo?.user?.role !== 'admin') {
-      this.router.navigate(['']);
+      this.router.navigate(['/login']);
       return false;
     }
     return true;
   }
-
 }
