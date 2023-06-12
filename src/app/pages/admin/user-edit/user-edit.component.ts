954550555e18d5351a1135ef8226c942719a99f3
Tran Hai Duong <haiduong12122003@gmail.com>
2023-06-12 22:30:02 +0700

    fix: user

--- src/app/pages/admin/user-edit/user-edit.component.ts
+++ src/app/pages/admin/user-edit/user-edit.component.ts
@@ -7,34 +7,40 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.scss']
+  styleUrls: ['./user-edit.component.scss'],
 })
 export class UserEditComponent {
   user!: IUser;
   userForm = this.formbuilder.group({
     name: [''],
-    email: ['']
-  })
-  constructor(private userService: UserService, private router: ActivatedRoute, private formbuilder: FormBuilder,private router1:Router) {
-    this.router.paramMap.subscribe(params => {
-      const id = params.get('id')
-      this.userService.getUserById(id).subscribe(data => {
-        this.user = data.user
+    email: [''],
+  });
+  constructor(
+    private userService: UserService,
+    private router: ActivatedRoute,
+    private formbuilder: FormBuilder,
+    private router1: Router
+  ) {
+    this.router.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      this.userService.getUserById(id).subscribe((data) => {
+        this.user = data.user;
         this.userForm.patchValue({
           name: data.user.name,
-          email: data.user.email
-        })
-      })
-    })
+          email: data.user.email,
+        });
+      });
+    });
   }
-  submitHandler(){
-    const newUser={
-      _id:this.user._id,
-      name:this.userForm.value.name || '',
-      email:this.userForm.value.email || ''
-    }
-    this.userService.updateUser(newUser).subscribe(()=>{
+  submitHandler() {
+    const newUser = {
+      _id: this.user._id,
+      name: this.userForm.value.name || '',
+      email: this.userForm.value.email || '',
+    };
+    this.userService.updateUser(newUser).subscribe(() => {
+      alert('Cập nhật user thành công ✅');
       this.router1.navigateByUrl('/admin/user');
-    })
+    });
   }
 }
